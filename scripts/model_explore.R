7681eb1e63178915edce2ab16c93aec8d21b0058
Allie Caughman <acaughman@ucsb.edu>
2022-02-11 21:09:55 -0800

    begin tests

--- scripts/model_explore.R
+++ scripts/model_explore.R
@@ -9,10 +9,10 @@ library(patchwork)
 NUM.reps <- 1 # The number of replicate simulations to run
 NUM.gens.pre.fishing <- 25 # The number of generations before any fishery
 NUM.gens.pre.reserve <- 50 # The number of generations of fishing before reserves are installed
-NUM.gens.post.reserve <- 50 # The number of generations with the reserve installed
+NUM.gens.post.reserve <- 75 # The number of generations with the reserve installed
 
-NS.patches <- 12 # the number of patches on the north-south axis
-EW.patches <- 12 # the number of patches on the east-west axis
+NS.patches <- 20 # the number of patches on the north-south axis
+EW.patches <- 20 # the number of patches on the east-west axis
 patch.size <- 100 # the width and height of each grid cell in nautical miles (COULD BE METERS?)
 ## View the "world" coordinates:
 view.world <- array(seq(1,NS.patches*EW.patches),c(NS.patches,EW.patches))
@@ -29,7 +29,7 @@ fished.factor <- 0.8
 #fished <- fished.factor*(1-s) # Fishing mortalty: the proportion of adults that get fished per year
 fished <- fished.factor
 buffer.fished <- 0 #buffer fishing pressure (lower than total = buffer zone, higher than total = fishing the line)
-reserves.at <- c(66,53,65,77,54,78,55,67,79) # This determines which patches are marine reserves. Should be a list: e.g., for one reserve, c(369,370,371,372,389,390,391,392,409,410,411,412,429,430,431,432)
+reserves.at <- c(193,213,194,214) # This determines which patches are marine reserves. Should be a list: e.g., for one reserve, c(369,370,371,372,389,390,391,392,409,410,411,412,429,430,431,432)
 buffer.at <- c()
 bold.mover.distance <- 80 # Individuals with AA genotype move this distance on average every year, in nautical miles
 lazy.mover.distance <- 60 # Individuals with aa genotype move this distance on average every year, in nautical miles
@@ -501,7 +501,7 @@ output_df = output_df %>%
   mutate(lat = as.numeric(lat)) %>% 
   mutate(lon = as.numeric(lon))
 
-#write_csv(output_df, here("intermediate_data" , "test1.csv"))
+#write_csv(output_df, here("test_output" , "2x28F.csv"))
 
 
 #Summarize pop size and frequency by genotype
@@ -517,11 +517,11 @@ pop_sum = output_df %>%
 output_sum = full_join(geno_sum, pop_sum) %>%
   mutate(freq = geno_pop_sum/pop_sum) 
 
-#write_csv(output_sum, here("intermediate_data" , "freq_test1.csv"))
+#write_csv(output_sum, here("test_output", "2x28F.csv"))
 
 
 plot_sum = output_sum %>% 
-  filter(generation %in% c(75, 100, 125)) %>% 
+  filter(generation %in% c(75, 100, 125, 150)) %>% 
   mutate(generation = as.numeric(generation))
 
 p1 = ggplot(plot_sum, aes(lon, lat, color = freq, fill = freq)) +
@@ -538,3 +538,7 @@ p2 = ggplot(plot_sum, aes(lon, lat, color = geno_pop_sum, fill = geno_pop_sum)) 
 
 p2 / p1
 
+plot = p2 / p1
+
+ggsave(plot, file=paste0("2x28F.png"), 
+       path = here("figs"))
