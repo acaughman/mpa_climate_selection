dd28d3c3a16e9c9fb677b784615e76b48830700d
Allie Caughman <acaughman@ucsb.edu>
2022-02-12 09:28:47 -0800

    climate grid set up completed

--- scripts/climate_grid_setup.R
+++ scripts/climate_grid_setup.R
@@ -1,21 +1,34 @@
-library(OpenImageR)
+library(tidyverse)
 
-SST.patches <- array(0, c(32, 8, 1))
-mean_SST = 28
+years = 15
+NS.patches = 32
+EW.patches = 8
 
-for (lat in 1:32) {
-  for(lon in 1:8) {
-    SST = rnorm(1, mean = mean_SST, sd = 0.5)
-    SST.patches[lat,lon,1] = SST
+SST.patches <- array(0, c(NS.patches, EW.patches, years))
+start_SST = 28 + 32*.1
+
+for (i in 1:years) {
+  SST = start_SST
+  for (lat in 1:NS.patches) {
+    SST.patches[lat,,i] = SST
+    SST = SST - .1
   }
+  start_SST = start_SST + rnorm(1, mean = 0.018, sd = 1)
 }
 
-SSTdf = SST.patches %>% 
-  as.data.frame() 
+output_df = data.frame() #create dataframe to hold results
 
-SSTdf$lat = c(1:32)
+for(b in 1:years) {
+  SSTdf = SST.patches[,,b] %>% 
+    as.data.frame() 
+  
+  SSTdf$lat = c(1:NS.patches)
+  SSTdf$year = paste0(b)
+  
+  output_df = bind_rows(output_df, SSTdf)
+}
 
-SSTdf = SSTdf %>% 
+SSTdf = output_df %>% 
   pivot_longer(V1:V8,
                names_to = "lon",
                values_to = "sst") %>% 
@@ -28,11 +41,13 @@ SSTdf = SSTdf %>%
     lon == "V6" ~ 6,
     lon == "V7" ~ 7,
     lon == "V8" ~ 8
-  ))
+  )) %>% 
+  mutate(year = as.factor(year))
 
 ggplot(SSTdf, aes(lon, lat, fill = sst)) +
   geom_tile() +
   labs(x = "Longitude", y = "Latitude", fill = "SST") +
   theme_bw() +
-  scale_fill_gradient2(low = "white", high = "midnightblue", mid = "lightskyblue", midpoint = 28)
+  scale_fill_gradient2(low = "white", high = "midnightblue", mid = "lightskyblue", midpoint = 30) +
+  facet_grid(~year)
 
