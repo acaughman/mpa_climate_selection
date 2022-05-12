b4a7c5a3c999e5744b883683f479c77a7a6d4583
Allie Caughman <acaughman@ucsb.edu>
2022-05-12 11:57:13 -0700

    set up for buffer code testing

--- scripts/playground.R
+++ scripts/playground.R
@@ -8,7 +8,60 @@ library(reticulate)
 seed = 42
 addTaskCallback(function(...) {set.seed(seed);TRUE})
 
+############################################################################
+## BUFFER CODE PLAY ##########################################################
 
+fishing <- function(pop,gen) {
+  if(gen <= pre.reserve.gens+pre.fishing.gens) {
+    each.patch.pop <- array(0,c(NS.patches,EW.patches))
+    for(i in 2:NUM.age.classes) {
+      for(j in 1:NUM.sexes) {
+        for(k in 1:NUM.genotypes) {
+          each.patch.pop[,] <- each.patch.pop[,] + pop[,,i,j,k]
+        }
+      }
+    }
+    mean.per.patch.pop <- mean(each.patch.pop)
+    ff <- mean.per.patch.pop*(1/fished-1)
+    patch.pop <- rowSums(pop[,,c(2,3),,], dims = 2)
+    f <- patch.pop/(ff+patch.pop)
+    for(i in 2:NUM.age.classes) {
+      for(j in 1:NUM.sexes) {
+        for(k in 1:NUM.genotypes) {
+          pop[,,i,j,k] <- Reshape(rbinom(NS.patches * EW.patches,pop[,,i,j,k],(1-f)), NS.patches, EW.patches)
+        }
+      }
+    }
+  }
+  if(gen > pre.reserve.gens+pre.fishing.gens) {
+    reserve.area <- sum(reserve.patches)/(NS.patches*EW.patches)
+    buffer.area <- sum(buffer.patches)/(NS.patches*EW.patches)
+    fished.adj <- (fished - (buffer.area*buffer.fished)) * 1/(1-(reserve.area + buffer.area))
+    each.patch.pop <- array(0,c(NS.patches,EW.patches))
+    for(i in 2:NUM.age.classes) {
+      for(j in 1:NUM.sexes) {
+        for(k in 1:NUM.genotypes) {
+          each.patch.pop[,] <- each.patch.pop[,] + pop[,,i,j,k]
+        }
+      }
+    }
+    each.patch.pop = ifelse(reserve.patches == 1, NaN, each.patch.pop)
+    mean.per.patch.pop <- mean(each.patch.pop,na.rm=TRUE)
+    ff <- mean.per.patch.pop*(1/fished.adj-1)
+    patch.pop <- rowSums(pop[,,c(2,3),,], dims=2)
+    patch.pop = ifelse(reserve.patches == 1, NaN, patch.pop)
+    f <- patch.pop/(ff+patch.pop)
+    for(i in 2:NUM.age.classes) {
+      for(j in 1:NUM.sexes) {
+        for(k in 1:NUM.genotypes) {
+          fished.array = Reshape(rbinom(NS.patches * EW.patches,pop[,,i,j,k],(1-f)), NS.patches, EW.patches)
+          pop[,,i,j,k] <- ifelse(is.na(fished.array[,]),pop[,,i,j,k],fished.array[,])
+        }
+      }
+    }
+  }
+  return(pop)
+}
 
 ############################################################################
 ## THE SIMULATION ##########################################################
