040ac9db026fd5af2f42efceca5000cb3e026fd2
Allie Caughman <acaughman@ucsb.edu>
2022-05-20 10:54:48 -0700

    initial commit

---
+++ scripts/old/fish_param_collect.R
@@ -0,0 +1,23 @@
+library(rfishbase)
+library(tidyverse)
+
+data = read_csv(here::here("species.csv"))
+validate_names(data$species)
+
+fish = load_taxa()
+
+species = species(fish$Species)
+stocks = stocks(fish$Species)
+mat = maturity(fish$Species)
+fec = fecundity(fish$Species)
+pop = popgrowth(fish$Species)
+
+dat = full_join(species, stocks)
+dat = full_join(dat, mat)
+dat = full_join(dat, fec)
+dat = full_join(dat, pop) %>% 
+  filter(Species %in% data$species) %>% 
+  select(Species, M,
+         FecundityMax,tm,AgeMatMin,
+         TempPreferred,TempPref50,
+         TempMin,TempMax)
