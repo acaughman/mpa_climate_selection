1dd2e754773058c6e2ed63b1e92cbdbbd8517a89
Allie Caughman <acaughman@ucsb.edu>
2022-05-24 15:41:09 -0700

    ready for full no climate models

--- scripts/data_processing.R
+++ scripts/data_processing.R
@@ -14,7 +14,7 @@ NUM.gens.pre.reserve <- 25 # The number of generations of fishing before reserve
 NUM.gens.post.reserve <- 100 # The number of generations with the reserve installed
 gens = NUM.gens.pre.fishing+NUM.gens.pre.reserve+NUM.gens.post.reserve
 
-load(file = here::here("data", "3x3NoClimate6F.rda"))
+load(file = here::here("data", "3x3null8F.rda"))
 
 # Output results into a dataframe
 output_df = data.frame() #create dataframe to hold results
