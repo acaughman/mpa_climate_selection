0e997273d9f9554a121686c89fd23626f8714b18
Allie Caughman <acaughman@ucsb.edu>
2022-06-11 22:24:20 -0700

    parallel not faster

--- scripts/playground.R
+++ scripts/playground.R
@@ -1,6 +1,10 @@
 library(foreach)
 library(doParallel)
 
+addTaskCallback(function(...) {set.seed(42);TRUE})
+options(warn=-1)
+options(dplyr.summarise.inform = FALSE)
+
 no_cores <- detectCores() - 1  
 cl <- makeCluster(no_cores, type="PSOCK")  
 registerDoParallel(cl)  
@@ -22,67 +26,77 @@ move_par = function(pop) {
         if(k == 2) { mean.dist <- Het.movers }
         if(k == 3) { mean.dist <- Hom.a.movers }
         out = foreach(i=2:NUM.age.classes) %:% 
-          foreach(j=1:NUM.sexes) %do% {
-            # movers are subtracted from the present grid cell
-            move.array[lat,lon,i,j,k] <- move.array[lat,lon,i,j,k] - pop[lat,lon,i,j,k]
-            # determine the distribution of movement distances in nautical miles:
-            dist <- rnbinom(pop[lat,lon,i,j,k], mu = mean.dist, size = Herit)
-            # determine the direction of each move
-            theta <- runif(pop[lat,lon,i,j,k],0,2*pi)
-            # bias this movement in the north-south direction (along coasts) if this is a great white shark simulation (otherwise, comment out the next three lines):
-            f.adj <- function(x, u) x-cos(x)*sin(x) - u
-            my.uniroot <- function(x) uniroot(f.adj, c(0, 2*pi), tol = 0.0001, u = x)$root
-            theta <- vapply(theta, my.uniroot, numeric(1))
-            # convert direction and distance into a distance in the x-direction (longitude)
-            x <- cos(theta)*dist
-            # bounce off edges (assume fish start in centre of cell)
-            for(m in 1:length(x)) {
-              miss_x.edges <- FALSE
-              while(miss_x.edges==FALSE) {
-                if(x[m] <= patch.size*(EW.patches-lon)+patch.size/2) {
-                  if(x[m] >= patch.size/2-lon*patch.size) {
-                    miss_x.edges <- TRUE }
-                }
-                if(x[m] > patch.size*(EW.patches-lon)+patch.size/2) {
-                  x[m] <- -(x[m]-2*(patch.size*(EW.patches-lon)+patch.size/2))
-                  # distance penalty for hitting an edge
-                  #x[m] <- x[m] + 1
-                }
-                if(x[m] < patch.size/2-lon*patch.size) {
-                  x[m] <- -(x[m]-2*(patch.size/2-lon*patch.size))
-                  # distance penalty for hitting an edge
-                  #x[m] <- x[m] - 1
-                }
-              }
-            }
-            # convert direction and distance into a distance in the y-direction (latitude)
-            y <- sin(theta)*dist
-            # bounce off edges (assume fish start in centre of cell)
-            for(m in 1:length(y)) {
-              miss_y.edges <- FALSE
-              while(miss_y.edges==FALSE) {
-                if(y[m] <= patch.size*(NS.patches-lat)+patch.size/2) {
-                  if(y[m] >= patch.size/2-lat*patch.size) {
-                    miss_y.edges <- TRUE }
-                }
-                if(y[m] > patch.size*(NS.patches-lat)+patch.size/2) {
-                  y[m] <- y[m] - (patch.size * NS.patches)
-                  # distance penalty for hitting an edge
-                  #y[m] <- y[m] + 1
-                }
-                if(y[m] < patch.size/2-lat*patch.size) {
-                  y[m] <- y[m] + (patch.size * NS.patches)
-                  # distance penalty for hitting an edge
-                  #y[m] <- y[m] - 1
+          foreach(j=1:NUM.sexes) %dopar% {
+            if(pop[lat,lon,i,j,k] > 0) {
+              patch.size = 100
+              EW.patches = 20
+              NS.patches = 100
+              # movers are subtracted from the present grid cell
+              move.array[lat,lon,i,j,k] <- move.array[lat,lon,i,j,k] - pop[lat,lon,i,j,k]
+              # determine the distribution of movement distances in nautical miles:
+              dist <- rnbinom(pop[lat,lon,i,j,k], mu = mean.dist, size = Herit)
+              # determine the direction of each move
+              theta <- runif(pop[lat,lon,i,j,k],0,2*pi)
+              # bias this movement in the north-south direction (along coasts) if this is a great white shark simulation (otherwise, comment out the next three lines):
+              f.adj <- function(x, u) x-cos(x)*sin(x) - u
+              my.uniroot <- function(x) uniroot(f.adj, c(0, 2*pi), tol = 0.0001, u = x)$root
+              theta <- vapply(theta, my.uniroot, numeric(1))
+              # convert direction and distance into a distance in the x-direction (longitude)
+              x <- cos(theta)*dist
+              # bounce off edges (assume fish start in centre of cell)
+              for(m in 1:length(x)) {
+                miss_x.edges <- FALSE
+                while(miss_x.edges==FALSE) {
+                  if(x[m] <= patch.size*(EW.patches-lon)+patch.size/2) {
+                    if(x[m] >= patch.size/2-lon*patch.size) {
+                      miss_x.edges <- TRUE }
+                  }
+                  if(x[m] > patch.size*(EW.patches-lon)+patch.size/2) {
+                    x[m] <- -(x[m]-2*(patch.size*(EW.patches-lon)+patch.size/2))
+                    # distance penalty for hitting an edge
+                    #x[m] <- x[m] + 1
+                  }
+                  if(x[m] < patch.size/2-lon*patch.size) {
+                    x[m] <- -(x[m]-2*(patch.size/2-lon*patch.size))
+                    # distance penalty for hitting an edge
+                    #x[m] <- x[m] - 1
+                  }
                 }
               }
-            }
-            # convert movement distances into numbers of grid cells (assume fish start in centre of cell):
-            xy <- as.data.frame(cbind(x,y))
-          } 
+              # convert direction and distance into a distance in the y-direction (latitude)
+              y <- sin(theta)*dist
+              # bounce off edges (assume fish start in centre of cell)
+              for(m in 1:length(y)) {
+                miss_y.edges <- FALSE
+                while(miss_y.edges==FALSE) {
+                  if(y[m] <= patch.size*(NS.patches-lat)+patch.size/2) {
+                    if(y[m] >= patch.size/2-lat*patch.size) {
+                      miss_y.edges <- TRUE }
+                  }
+                  if(y[m] > patch.size*(NS.patches-lat)+patch.size/2) {
+                    y[m] <- y[m] - (patch.size * NS.patches)
+                    # distance penalty for hitting an edge
+                    #y[m] <- y[m] + 1
+                  }
+                  if(y[m] < patch.size/2-lat*patch.size) {
+                    y[m] <- y[m] + (patch.size * NS.patches)
+                    # distance penalty for hitting an edge
+                    #y[m] <- y[m] - 1
+                  }
+                }
+              }
+              # convert movement distances into numbers of grid cells (assume fish start in centre of cell):
+              xy <- as.data.frame(cbind(x,y))
+            } else {
+              NULL
+            } 
+          }
         for(i in 2:NUM.age.classes) {
           for(j in 1:NUM.sexes) {
             xy = out[[i-1]][[j]]
+            if (is.null(xy)) {
+              break
+            }
             hx <- hist(xy$x, breaks = seq(round(min(xy$x),-2)-patch.size/2,round(max(xy$x),-2)+patch.size/2,patch.size), plot = FALSE)
             xbreaks <- hx$breaks
             xmids <- hx$mids
@@ -107,7 +121,7 @@ move_par = function(pop) {
   }
 }
 
-reps <- 1
+reps <- 5
 
 pre.fishing.gens <- NUM.gens.pre.fishing
 pre.reserve.gens <- NUM.gens.pre.reserve
@@ -163,6 +177,8 @@ for(rep in 1:reps) {
 gc()
 
 end_time_par <- Sys.time()
+
 end_time - start_time
+end_time_par - start_time_par
 
 stopImplicitCluster()
