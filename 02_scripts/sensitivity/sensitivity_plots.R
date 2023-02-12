06587c9004b9b30b4ec2446f61d786ffb4a3dfa4
Allie Caughman <acaughman@ucsb.edu>
2023-02-11 22:36:49 -0800

    update sensitvity analysis

--- 02_scripts/sensitivity/sensitivity_plots.R
+++ 02_scripts/sensitivity/sensitivity_plots.R
@@ -4,27 +4,27 @@ library(lemon)
 
 # Read in Data ------------------------------------------------------------
 
-df1 <- read_csv(here::here("sensitivity_analysis", "movement_rate", "mean_shock_large_sum.csv")) %>%
+df1 <- read_csv(here::here("sensitivity_analysis", "fishing_pressure", "mean_shock_med_sum.csv")) %>%
   mutate(climate = "Mean Shock") %>%
   mutate(dd = as.factor(dd)) %>%
   mutate(max_temp = as.character(max_temp))
 
-df2 <- read_csv(here::here("sensitivity_analysis", "movement_rate", "mean_large_sum.csv")) %>%
+df2 <- read_csv(here::here("sensitivity_analysis", "fishing_pressure", "mean_med_sum.csv")) %>%
   mutate(climate = "Mean") %>%
   mutate(dd = as.factor(dd)) %>%
   mutate(max_temp = as.character(max_temp))
 
-df3 <- read_csv(here::here("sensitivity_analysis", "movement_rate", "shock_large_sum.csv")) %>%
+df3 <- read_csv(here::here("sensitivity_analysis", "fishing_pressure", "shock_med_sum.csv")) %>%
   mutate(climate = "Shock") %>%
   mutate(dd = as.factor(dd)) %>%
   mutate(max_temp = as.character(max_temp))
 
-df4 <- read_csv(here::here("sensitivity_analysis", "movement_rate", "enso_large_sum.csv")) %>%
+df4 <- read_csv(here::here("sensitivity_analysis", "fishing_pressure", "enso_med_sum.csv")) %>%
   mutate(climate = "Enso") %>%
   mutate(dd = as.factor(dd)) %>%
   mutate(max_temp = as.character(max_temp))
 
-df5 <- read_csv(here::here("sensitivity_analysis", "movement_rate", "null_large_sum.csv")) %>%
+df5 <- read_csv(here::here("sensitivity_analysis", "fishing_pressure", "null_med_sum.csv")) %>%
   mutate(climate = "Null") %>%
   mutate(dd = as.factor(dd)) %>%
   mutate(max_temp = as.character(max_temp))
@@ -54,9 +54,9 @@ p <- ggplot(data, aes(generation, location_sum)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   scale_color_viridis_d() +
   theme(panel.grid.minor = element_blank()) +
-  ylim(c(0, 150000))
+  ylim(c(0, 10000))
 
-ggsave(reposition_legend(p, 'center', panel='panel-3-2'), file = paste0("large.pdf"), path = here::here("sensitivity_analysis", "movement_rate"), height = 8, width = 15)
+#ggsave(reposition_legend(p, 'center', panel='panel-3-2'), file = paste0("med.pdf"), path = here::here("sensitivity_analysis", "fishing_pressure"), height = 8, width = 15)
 # reposition_legend(p, 'center', panel='panel-3-2')
 
 # Frequency Plot ----------------------------------------------------------
@@ -64,7 +64,7 @@ ggsave(reposition_legend(p, 'center', panel='panel-3-2'), file = paste0("large.p
 p <- ggplot(data, aes(generation, freq_avg)) +
   geom_line(aes(color = dd)) +
   theme_bw() +
-  facet_grid(climate ~ genotype) +
+  facet_grid(genotype ~ climate) +
   labs(
     x = "Year",
     y = "Allele Frequency",
@@ -74,8 +74,8 @@ p <- ggplot(data, aes(generation, freq_avg)) +
   geom_vline(xintercept = 26, alpha = 0.3) +
   # geom_vline(xintercept = c(136,66,124,79,35,143,130,71,82,90,25,73,80,103,51,34,142,40,86,141,97,137,113,144,122,132,149,129), alpha = 0.3, color= "red") +
   scale_x_continuous(breaks = c(16, 26), labels = c("fishing starts", "MPA establishment")) +
-  theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
+  theme(axis.text.x = element_text(angle = 0, hjust = 1)) +
   scale_color_viridis_d() +
   theme(panel.grid.minor = element_blank())
 
-# ggsave(p, file = paste0("large.pdf"), path = here::here("sensitivity_analysis", "movement_rate"), height = 8, width = 15)
+ggsave(p, file = paste0("med.pdf"), path = here::here("sensitivity_analysis", "fishing_pressure"), height = 8, width = 15)
